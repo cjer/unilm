10f4bb01d2eba956b35c79f189d0dcf07dc6c5c1
cjer <dbareket@gmail.com>
2023-12-27 20:49:42 +0200

    handle none report_to

--- simlm/src/train_biencoder.py
+++ simlm/src/train_biencoder.py
@@ -47,6 +47,7 @@ def main():
     parser = HfArgumentParser((Arguments,))
     args: Arguments = parser.parse_args_into_dataclasses()[0]
     _common_setup(args)
+    args.report_to = None if args.report_to == 'none' else args.report_to
     logger.info('Args={}'.format(str(args)))
 
     tokenizer: PreTrainedTokenizerFast = AutoTokenizer.from_pretrained(args.model_name_or_path)
